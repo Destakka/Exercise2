895f8cf26ecd0c8e7d9ebb40b212fc5de5df6127
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-11-04 16:00:10 +0700

    add void read

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -12,5 +12,19 @@ namespace Exercise2
             cmp_count = 0;
             mov_count = 0;
         }
+        void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of element in the array:");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\n Array can have maximum 20 elements. \n");
+            }
+
+        }
     }
 }
