fb481c6655470143f1a47433629a482ed94ad0d3
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-11-04 15:51:12 +0700

    Add class list

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -2,5 +2,15 @@
 
 namespace Exercise2
 {
-
+    class List
+    {
+        private int[] arr = new int[20];
+        private int[] sorted = new int[20];
+        private int cmp_count, mov_count, n;
+        public List()
+        {
+            cmp_count = 0;
+            mov_count = 0;
+        }
+    }
 }
